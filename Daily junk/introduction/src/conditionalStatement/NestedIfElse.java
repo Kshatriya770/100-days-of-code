c61b806bffb7cf2d70ec6e6e9d492ba4fa53db37
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-09-15 09:13:25 +0530

    nested ifelse

---
+++ Daily junk/introduction/src/conditionalStatement/NestedIfElse.java
@@ -0,0 +1,30 @@
+package conditionalStatement;
+
+public class NestedIfElse {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 55;
+		int c = 70;
+		
+		int result = 0;
+		
+		if(a>b) {
+			if (a>c) {
+				result = a;
+			}
+			else 
+				result = c;
+		} else {
+			if(b>c) {
+				result = b;
+			} 
+			else {
+				result = c;
+			}
+		}
+		System.out.println("the greater no is : " +result);
+	}
+
+}
