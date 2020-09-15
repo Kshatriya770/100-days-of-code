d9f3684fa6700581d2220bd1c87e56c973f6342c
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-09-15 15:38:37 +0530

    2 case in 1

--- Daily junk/introduction/src/conditionalStatement/SwitchCase.java
+++ Daily junk/introduction/src/conditionalStatement/SwitchCase.java
@@ -6,12 +6,15 @@ public class SwitchCase {
 		int dayOfWeek = 4;
 		
 		switch (dayOfWeek) {
+//		case 1:
+//			System.out.println("I am on break");
+//			break;
+//		case 2:
+//			System.out.println("I am on break");
+//			break;
 		case 1:
-			System.out.println("I am on break");
-			break;
 		case 2:
-			System.out.println("I am on break");
-			break;
+			System.out.println("xyz");
 		default:
 			System.out.println("i dont know ");
 	}
