712e6b17705f07b44d18af84ffa9c5be391dd932
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-09-15 14:59:07 +0530

    Day 2 switch

---
+++ Daily junk/introduction/src/conditionalStatement/SwitchCase.java
@@ -0,0 +1,22 @@
+package conditionalStatement;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		int dayOfWeek = 4;
+		
+		switch (dayOfWeek) {
+		case 1:
+			System.out.println("I am on break");
+		
+		case 2:
+			System.out.println("I am on break");
+		
+		default:
+			System.out.println("i dont know ");
+	}
+
+
+	}
+
+}
