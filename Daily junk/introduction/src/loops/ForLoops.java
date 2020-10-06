ec929390f4e6a2b8babdd60e998060ede36b875f
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-07 00:00:19 +0530

    loops

---
+++ Daily junk/introduction/src/loops/ForLoops.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 100; i = i +1) {
+			System.out.println("Hello World" + i);
+		}
+
+	}
+
+}
