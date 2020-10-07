b10848ee8869ae22c6d74b4a767c5225ec916a2f
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-10-08 01:14:30 +0530

    pattern 2 reverse

--- Daily junk/introduction/src/loops/Pattern2.java
+++ Daily junk/introduction/src/loops/Pattern2.java
@@ -9,7 +9,7 @@ public class Pattern2 {
 		int n = sc.nextInt();
 		
 		for(int i = 1; i <= n ; i++) {
-			for(int j = 1; j <= i; j++) {
+			for(int j = 1; j <= n-i+1; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
